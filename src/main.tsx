b62cee6a0bfaeb0cb47184eef26112ef43511ab8
kevinccbsg <kevinccbsg@gmail.com>
2025-11-08 00:20:49 +0100

    feat: add basic tests

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import './index.css'
 import { RouterProvider } from "react-router";
 import router from './AppRoutes';
 
+if (import.meta.env.DEV) {
+  // You choose how to load the tests; this example uses Vite's glob import
+  const testModules = import.meta.glob("./**/*.twd.test.ts");
+  const { initTests, TWDSidebar } = await import('twd-js');
+  // You need to pass the test modules, the sidebar component, and createRoot function
+  initTests(testModules, <TWDSidebar open={true} position="left" />, createRoot);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
---
+++ src/twd-tests/helloWorld.twd.test.ts
@@ -0,0 +1,8 @@
+import { twd } from "twd-js";
+import { describe, it } from "twd-js/runner";
+
+describe("Hello World Page", () => {
+  it("should display the welcome title and counter button", async () => {
+    await twd.visit('/');
+  });
+});
