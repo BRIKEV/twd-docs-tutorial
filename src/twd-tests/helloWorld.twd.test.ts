2d8bc1bd3f663d84bdf21b39998453147b5f8e3d
kevinccbsg <kevinccbsg@gmail.com>
2025-11-08 22:24:41 +0100

    feat: complete assertion examples

--- src/twd-tests/helloWorld.twd.test.ts
+++ src/twd-tests/helloWorld.twd.test.ts
@@ -1,8 +1,23 @@
-import { twd } from "twd-js";
+import { twd, userEvent } from "twd-js";
 import { describe, it } from "twd-js/runner";
 
 describe("Hello World Page", () => {
   it("should display the welcome title and counter button", async () => {
-    await twd.visit('/');
+    await twd.visit("/");
+    
+    const title = await twd.get("[data-testid='welcome-title']");
+    title.should("be.visible").should("have.text", "Welcome to TWD");
+    
+    const counterButton = await twd.get("[data-testid='counter-button']");
+    counterButton.should("be.visible").should("have.text", "Count is 0");
+    
+    await userEvent.click(counterButton.el);
+    counterButton.should("have.text", "Count is 1");
+    
+    await userEvent.click(counterButton.el);
+    counterButton.should("have.text", "Count is 2");
+    
+    await userEvent.click(counterButton.el);
+    counterButton.should("have.text", "Count is 3");
   });
 });
