9109925fca0c89507aa70a99ac827299a6fc0e74
kevinccbsg <kevinccbsg@gmail.com>
2025-11-08 23:58:48 +0100

    feat: install service worker

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,17 @@ import router from './AppRoutes';
 if (import.meta.env.DEV) {
   // You choose how to load the tests; this example uses Vite's glob import
   const testModules = import.meta.glob("./**/*.twd.test.ts");
-  const { initTests, TWDSidebar } = await import('twd-js');
+  const { initTests, twd, TWDSidebar } = await import('twd-js');
   // You need to pass the test modules, the sidebar component, and createRoot function
   initTests(testModules, <TWDSidebar open={true} position="left" />, createRoot);
+  // if you want to use mock requests, you can initialize it here
+  twd.initRequestMocking()
+    .then(() => {
+      console.log("Request mocking initialized");
+    })
+    .catch((err) => {
+      console.error("Error initializing request mocking:", err);
+    });
 }
 
 createRoot(document.getElementById('root')!).render(
