d50d44a76b186344808b4778016ed64c6df2c0dc
kevinccbsg <kevinccbsg@gmail.com>
2026-05-25 20:09:12 +0200

    chore: add basic configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 /// <reference types="vitest" />
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
+import { twd } from 'twd-js/vite-plugin';
+import { twdRemote } from 'twd-relay/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    // changes
+    twd({
+      testFilePattern: '/**/*.twd.test.{ts,tsx}',
+      search: true,
+    }),
+    twdRemote() as PluginOption,
   ],
   resolve: {
     alias: {
